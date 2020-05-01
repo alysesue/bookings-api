40adfc959d133314613e95fb5f462cc12eaa651b
Will <wnogueira@palo-it.com>
2020-05-01 12:46:09 +1000

    webpack nightmare

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var service_node12_webpack_config_1 = require("mol-lib-config/shared-config/service.node12.webpack.config");
+var config = {
+    name: "bookingsg-api",
+    dir: "./"
+};
+var webPackConfig = service_node12_webpack_config_1.createBaseServiceConfig(config);
+exports["default"] = webPackConfig;
--- webpack.config.ts
+++
@@ -1,9 +0,0 @@
-import { Config, createBaseServiceConfig } from "mol-lib-config/shared-config/service.node12.webpack.config";
-
-const config: Config = {
-	name: "bookingsg-api",
-	dir: "./",
-};
-const webPackConfig = createBaseServiceConfig(config);
-
-export default webPackConfig;
