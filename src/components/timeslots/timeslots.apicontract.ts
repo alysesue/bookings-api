d8d47257733a3fde6b1269a90b97fc797a84a7bf
Will <wnogueira@palo-it.com>
2020-05-04 17:51:05 +1000

    timeslot api contract

---
+++ src/components/timeslots/timeslots.apicontract.ts
@@ -0,0 +1,14 @@
+export class TimeslotsFilter {
+	public startDate: Date
+	public isAvailable: boolean;
+}
+
+export class TimeslotModel {
+	public id: number;
+
+	public startDatetime: Date;
+	public endDatetime: Date;
+
+	public calendarCount: number;
+}
+
