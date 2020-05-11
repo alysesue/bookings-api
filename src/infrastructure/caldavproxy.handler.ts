885d490a9ea4ca37d3b98878b4f4333c51a7e58e
Will <wnogueira@palo-it.com>
2020-05-11 11:48:21 +1000

    PR fix

--- src/infrastructure/caldavproxy.handler.ts
+++ src/infrastructure/caldavproxy.handler.ts
@@ -1,12 +1,15 @@
 import * as Koa from "koa";
 import * as KoaProxy from 'koa-proxy';
-import { Container, Singleton } from 'typescript-ioc';
+import { Container, Inject, Singleton } from 'typescript-ioc';
 import { GoogleCalendarService } from '../googleapi/google.calendar.service';
 
 @Singleton
 export class CalDavProxyHandler {
-	private koaProxy: any;
 
+	@Inject
+	private calendarService: GoogleCalendarService;
+
+	private koaProxy: any;
 	private _initialized: boolean = false;
 	public httpProtocol: string = null;
 	public httpHost: string = null;
@@ -17,15 +20,10 @@ export class CalDavProxyHandler {
 		});
 	}
 
-	private async getAccessToken(): Promise<string> {
-		const calendarService = Container.get(GoogleCalendarService);
-		return await calendarService.getAccessToken();
-	}
-
 	private async koaProxyWithAuth(ctx: Koa.Context, next: Koa.Next): Promise<any> {
 		ctx.originalUrl = ctx.url;
 
-		const accessToken = await this.getAccessToken();
+		const accessToken = await this.calendarService.getAccessToken();
 		ctx.header['Authorization'] = `Bearer ${accessToken}`;
 
 		return await this.koaProxy(ctx, next);
