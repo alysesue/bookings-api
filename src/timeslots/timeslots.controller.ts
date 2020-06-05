fdffca5764401cc89fff1a6b24907233e9c40d05
Janis Ardel <jardel@palo-it.com>
2020-06-05 12:19:51 +1000

    remove /** middleware

--- src/timeslots/timeslots.controller.ts
+++ src/timeslots/timeslots.controller.ts
@@ -1,22 +1,22 @@
 import { Inject } from "typescript-ioc";
-import { Controller, Get, Query, Route, Tags, } from "tsoa";
+import { Controller, Get, Header, Query, Route, Security, Tags, } from "tsoa";
 import { TimeslotResponse } from "./timeslots.apicontract";
 import { TimeslotsService } from './timeslots.service';
 import { DateHelper } from "../infrastructure/dateHelper";
 
-@Route("**/v1/timeslots")
+@Route("v1/timeslots")
 @Tags('Timeslots')
 export class TimeslotsController extends Controller {
 	@Inject
 	private timeslotsService: TimeslotsService;
 
 	@Get("availability")
-	public async getAggregatedTimeslots(@Query() startDate: Date, @Query() endDate: Date): Promise<TimeslotResponse[]> {
+	@Security("service")
+	public async getAggregatedTimeslots(@Query() startDate: Date, @Query() endDate: Date, @Header('x-api-service') _?): Promise<TimeslotResponse[]> {
 		// Parameters are created as UTC datetimes, treating them as local datetimes
 		startDate = DateHelper.UTCAsLocal(startDate);
 		endDate = DateHelper.UTCAsLocal(endDate);
 
-		const aggregated = await this.timeslotsService.getAggregatedTimeslots(startDate, endDate);
-		return aggregated;
+		return await this.timeslotsService.getAggregatedTimeslots(startDate, endDate);
 	}
 }
