33cfdde7368520c636960c70eaceddd710ab3c59
Will <wnogueira@palo-it.com>
2020-09-09 14:32:30 +1000

    citizen bookings fix

--- src/components/bookings/bookings.repository.ts
+++ src/components/bookings/bookings.repository.ts
@@ -14,20 +14,25 @@ export class BookingsRepository extends RepositoryBase<Booking> {
 		super(Booking);
 	}
 
-	private async createQueryForUser(_accessType: QueryAccessType): Promise<SelectQueryBuilder<Booking>> {
+	private async createUserVisibilityCondition(
+		alias: string,
+		_accessType: QueryAccessType,
+	): Promise<{ userCondition: string; userParams: object }> {
 		const user = await this.userContext.getCurrentUser();
 
-		const repository = await this.getRepository();
-		let query = repository.createQueryBuilder('booking');
-		if (user.isCitizen()) {
-			query = query.where('booking."_citizenUinFin" = :uinfin', { uinfin: user.singPassUser.UinFin });
-		}
-
-		return query;
+		return {
+			userCondition: `${alias}."_citizenUinFin" = :useruinfin`,
+			userParams: { useruinfin: user.singPassUser.UinFin },
+		};
 	}
 
 	public async getBooking(bookingId: number, accessType = QueryAccessType.Read): Promise<Booking> {
-		const query = (await this.createQueryForUser(accessType))
+		const { userCondition, userParams } = await this.createUserVisibilityCondition('booking', accessType);
+
+		const repository = await this.getRepository();
+		const query = repository
+			.createQueryBuilder('booking')
+			.where(userCondition, { ...userParams })
 			.leftJoinAndSelect('booking._serviceProvider', 'sp_relation')
 			.leftJoinAndSelect('booking._service', 'service_relation')
 			.where('booking."_id" = :id', { id: bookingId });
@@ -69,6 +74,8 @@ export class BookingsRepository extends RepositoryBase<Booking> {
 	}
 
 	public async search(request: BookingSearchQuery, accessType: QueryAccessType): Promise<Booking[]> {
+		const { userCondition, userParams } = await this.createUserVisibilityCondition('booking', accessType);
+
 		const serviceCondition = request.serviceId ? 'booking."_serviceId" = :serviceId' : '';
 
 		const serviceProviderCondition = request.serviceProviderId
@@ -83,9 +90,12 @@ export class BookingsRepository extends RepositoryBase<Booking> {
 
 		const dateRangeCondition = '(booking."_startDateTime" < :to AND booking."_endDateTime" > :from)';
 
-		const query = (await this.createQueryForUser(accessType))
+		const repository = await this.getRepository();
+		const query = repository
+			.createQueryBuilder('booking')
 			.where(
 				[
+					userCondition,
 					serviceCondition,
 					serviceProviderCondition,
 					dateRangeCondition,
@@ -95,6 +105,7 @@ export class BookingsRepository extends RepositoryBase<Booking> {
 					.filter((c) => c)
 					.join(' AND '),
 				{
+					...userParams,
 					serviceId: request.serviceId,
 					serviceProviderId: request.serviceProviderId,
 					from: request.from,
