9a964ca19fd1d4e376004d0735f326341c90bff2
Andrew Lai <laiandrew@hotmail.com>
2020-09-16 11:22:11 +0800

    Revert "[MOL-4303][MR|AL] Update mol-lib-config to use golden image"
    This reverts commit bfac2559b48ee08c00d507d03137fc88433d3b53.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,9 +4,6 @@
 
 - PLEASE WRITE CHANGELOGS
 
-## xxx
-- Updated mol-lib-config to use golden image
-
 ---
 
 ## 0.0.1 (use package.json version)
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 exports.__esModule = true;
-var service_node12_webpack_config_1 = require('mol-lib-config/shared-config/service.golden.node12.webpack.config');
+var service_node12_webpack_config_1 = require('mol-lib-config/shared-config/service.node12.webpack.config');
 var config = {
 	name: 'bookingsg',
 	dir: './',
@@ -11,11 +11,13 @@ var config = {
 var webPackConfig = service_node12_webpack_config_1.createBaseServiceConfig(config);
 webPackConfig.plugins.push(
 	new CopyPlugin({
-		patterns: [{
-			from: path.resolve(config.dir, 'swagger'),
-			to: './swagger',
-			toType: 'dir',
-		}, ],
+		patterns: [
+			{
+				from: path.resolve(config.dir, 'swagger'),
+				to: './swagger',
+				toType: 'dir',
+			},
+		],
 	}),
 );
 exports['default'] = webPackConfig;
