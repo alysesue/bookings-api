91c64e1b49fda08a3d1c30d5c882c0f5f27efd02
Will <wnogueira@palo-it.com>
2020-10-07 12:23:25 +1100

    fixed build

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 exports.__esModule = true;
-var service_node12_webpack_config_1 = require('mol-lib-config/shared-config/service.node12.webpack.config');
+var service_node12_webpack_config_1 = require('mol-lib-config/shared-config/service.golden.node12.webpack.config');
 var config = {
 	name: 'bookingsg',
 	dir: './',
