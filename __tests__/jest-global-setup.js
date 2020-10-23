5d495c38140a3ffcb87ef1e38fbf5b01906bbf7b
Adrien L <adrien@MacBook-Pro-3.local>
2020-10-23 11:12:06 +1100

    Add functional tests

---
+++ __tests__/jest-global-setup.js
@@ -0,0 +1,3 @@
+module.exports = async () => {
+	process.env.FUNCTIONAL_TEST_BASE_URL='http://localhost:3999/bookingsg/api/v1';
+};
---
+++ __tests__/jest.config.functional.js
@@ -0,0 +1,32 @@
+// =============================================================================
+// This creates a base jest config
+// =============================================================================
+
+module.exports = {
+	collectCoverageFrom: [
+		"**/src/**/*.{js,jsx,ts,tsx}",
+	],
+	coverageReporters: ["text-summary"],
+	coverageDirectory: "<rootDir>/__tests__/functional/coverage",
+	transform: {
+		".(js|ts|jsx|tsx)$": "ts-jest",
+	},
+	testMatch: [
+		"<rootDir>/__tests__/**/*.(spec|unit|test).(js|ts)?(x)",
+	],
+	modulePaths: [
+		"src",
+	],
+	moduleFileExtensions: [
+		"js",
+		"ts",
+		"jsx",
+		"tsx",
+	],
+	rootDir: "../",
+	verbose: true,
+	bail: true,
+	globalSetup: "./__tests__/jest-global-setup.js",
+	setupFiles: ["./__tests__/jest.setup.js"],
+	testTimeout: 10000
+};
