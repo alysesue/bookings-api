8cf6cc4c89a74f88c533dfa9cf09d9a3ec490333
Monica <mlam@palo-it.com>
2020-12-15 12:20:14 +1100

    merge tests

--- __tests__/functional/Bookings/adminPost.spec.ts
+++ __tests__/functional/Bookings/adminPost.spec.ts
@@ -5,7 +5,7 @@ import {
 	populateIndividualTimeslot
 } from '../../Populate/basic';
 
-describe('Bookings functional tests as admin- out of slots', () => {
+describe('Bookings functional tests as admin', () => {
 	const pgClient = new PgClient();
 	const startDateTime = new Date(2020, 12, 6, 8, 0);
 	const endDateTime = new Date(2020, 12, 6, 9, 0);
@@ -34,36 +34,10 @@ describe('Bookings functional tests as admin- out of slots', () => {
 		const adminCreateBookingOos = await OrganisationAdminRequestEndpointSG.create({ serviceId: result.service.id }).post(`/bookings/admin`, { body: { startDateTime: startDateTime, endDateTime: endDateTime, serviceProviderId: result.serviceProvider[0].id, citizenUinFin, citizenName, citizenEmail } });
 		expect(adminCreateBookingOos.statusCode).toEqual(201);
 	});
-});
-
-describe('Bookings functional tests as admin- with provided slots', () => {
-	const pgClient = new PgClient();
-	const startDateTime = new Date(Date.UTC(2020, 11, 6, 0, 0));
-	const endDateTime = new Date(Date.UTC(2020, 11, 6, 1, 0))
-	const citizenUinFin = 'S7429377H';
-	const citizenName = 'Jane';
-	const citizenEmail = 'jane@email.com';
-	let result;
-
-	beforeAll(async () => {
-		await pgClient.cleanAllTables();
-	});
-
-	afterAll(async () => {
-		await pgClient.close();
-	});
-
-	beforeEach(async () => {
-		result = await populateServiceAndServiceProvider({});
-		await populateIndividualTimeslot({ serviceProviderId: result.serviceProvider[0].id, weekDay: 0, startTime: "08:00", endTime: "09:00", capacity: 1 });
-	});
-
-	afterEach(async () => {
-		await pgClient.cleanAllTables();
-	});
-
 
 	it('admin should create booking in provided slot', async () => {
+		await populateIndividualTimeslot({ serviceProviderId: result.serviceProvider[0].id, weekDay: 0, startTime: "08:00", endTime: "09:00", capacity: 1 });
+
 		const adminCreateBooking = await OrganisationAdminRequestEndpointSG.create({ serviceId: result.service.id }).post(`/bookings/admin`, { body: { startDateTime, endDateTime, serviceProviderId: result.serviceProvider[0].id, citizenUinFin, citizenName, citizenEmail } });
 		expect(adminCreateBooking.statusCode).toEqual(201);
 	});
--- __tests__/functional/Bookings/post.spec.ts
+++ __tests__/functional/Bookings/post.spec.ts
@@ -37,36 +37,9 @@ describe('Bookings functional tests as citizen - out of slots', () => {
 		expect(citizenCancelBookingResponse.statusCode).toEqual(204);
 	});
 
-});
-
-describe('Bookings functional tests as citizen- with provided slots', () => {
-	const pgClient = new PgClient();
-	const startDateTime = new Date(Date.UTC(2020, 11, 6, 0, 0));
-	const endDateTime = new Date(Date.UTC(2020, 11, 6, 1, 0))
-	const citizenUinFin = 'S7429377H';
-	const citizenName = 'Jane';
-	const citizenEmail = 'jane@email.com';
-	let result;
-
-	beforeAll(async () => {
-		await pgClient.cleanAllTables();
-	});
-
-	afterAll(async () => {
-		await pgClient.close();
-	});
-
-	beforeEach(async () => {
-		result = await populateServiceAndServiceProvider({});
-		await populateIndividualTimeslot({ serviceProviderId: result.serviceProvider[0].id, weekDay: 0, startTime: "08:00", endTime: "09:00", capacity: 1 });
-	});
-
-	afterEach(async () => {
-		await pgClient.cleanAllTables();
-	});
-
-
 	it('citizen should create booking in provided slot', async () => {
+		await populateIndividualTimeslot({ serviceProviderId: result.serviceProvider[0].id, weekDay: 0, startTime: "08:00", endTime: "09:00", capacity: 1 });
+
 		const citizenCreateBooking = await CitizenRequestEndpointSG.create({ serviceId: result.service.id }).post(`/bookings`, { body: { token: "123321token", startDateTime, endDateTime, serviceProviderId: result.serviceProvider[0].id, citizenUinFin, citizenName, citizenEmail } });
 		expect(citizenCreateBooking.statusCode).toEqual(201);
 		const adminCreateBookingOos = await OrganisationAdminRequestEndpointSG.create({ serviceId: result.service.id }).post(`/bookings/admin`, { body: { startDateTime, endDateTime, serviceProviderId: result.serviceProvider[0].id, citizenUinFin, citizenName, citizenEmail } });
