5557041e98ce332d7ba027630ebacab16df29b64
asue <asue@palo-it.com>
2021-02-16 10:04:01 +1100

    Change migration export

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,3 +1,3 @@
 export { Init1610601065758 } from './1610601065758-init';
 export { InitUpdate1613015490544 } from './1613015490544-InitUpdate';
-export { AddIsStandAlone1613362765182 } from './1613362765182-addIsStandAlone';
+export { AddIsStandAlone1613362765182 } from './1613362765182-AddIsStandAlone';
