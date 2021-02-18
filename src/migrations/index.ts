a88c7fab67e26f9b0b21720fab7e0453eed05fd7
Will <wnogueira@palo-it.com>
2021-02-18 14:25:21 +1100

    migration fix

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,3 +1,2 @@
 export { Init1610601065758 } from './1610601065758-init';
-export { InitUpdate1613015490544 } from './1613015490544-InitUpdate';
 export { OrgConfiguration1613085942023 } from './1613085942023-OrgConfiguration';
