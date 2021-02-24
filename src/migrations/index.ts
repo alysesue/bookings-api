1fbd28584e1554f8cc69d572b8716db6829d01a0
Adrien L <adrien@ip-192-168-1-100.ap-southeast-1.compute.internal>
2021-02-24 15:30:59 +1100

    [AL] Change group mapping

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,2 +1,3 @@
 export { Init1610601065758 } from './1610601065758-init';
 export { OrgConfiguration1613085942023 } from './1613085942023-OrgConfiguration';
+export { PropertiesGroupMapping1614140319620 } from './1614140319620-PropertiesGroupMapping';
