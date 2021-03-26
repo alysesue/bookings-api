fb10b1fe0e966f1d6f1c0ce2abe2589d291ed321
Darrel <darrel@ip-192-168-1-17.ap-southeast-1.compute.internal>
2021-03-26 16:06:59 +0800

    [DC] added functional tests for SP auto assign

--- src/migrations/1616719301709-IsSpAutoAssigned.ts
+++ src/migrations/1616719301709-IsSpAutoAssigned.ts
@@ -4,12 +4,10 @@ export class IsSpAutoAssigned1616719301709 implements MigrationInterface {
 	public name = 'isSpAutoAssigned1616719301709';
 
 	public async up(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.query(`DROP INDEX "IDX_488976e66147a39155c77595f3"`);
 		await queryRunner.query(`ALTER TABLE "service" ADD "_isSpAutoAssigned" boolean NOT NULL DEFAULT false`);
 	}
 
 	public async down(queryRunner: QueryRunner): Promise<void> {
 		await queryRunner.query(`ALTER TABLE "service" DROP COLUMN "_isSpAutoAssigned"`);
-		await queryRunner.query(`CREATE INDEX "IDX_488976e66147a39155c77595f3" ON "booking_change_log" ("_userId") `);
 	}
 }
