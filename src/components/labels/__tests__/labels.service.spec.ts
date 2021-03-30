e6e74ba9211611f4af2420bc4ffbcea059268453
Darrel <darrel@ip-192-168-1-17.ap-southeast-1.compute.internal>
2021-03-30 14:24:52 +0800

    [DC] added unit test for labels not present

--- src/components/labels/__tests__/labels.service.spec.ts
+++ src/components/labels/__tests__/labels.service.spec.ts
@@ -3,6 +3,7 @@ import { LabelsRepository } from '../labels.repository';
 import { LabelsRepositoryMock } from '../__mocks__/labels.repository.mock';
 import { Label, Service } from '../../../models/entities';
 import { LabelsService } from '../labels.service';
+import { MOLErrorV2 } from 'mol-lib-api-contract';
 
 describe('Test labels service', () => {
 	beforeAll(() => {
@@ -14,5 +15,16 @@ describe('Test labels service', () => {
 		LabelsRepositoryMock.findMock.mockReturnValue(new Promise(() => [label]));
 		Container.get(LabelsService).verifyLabels([label], { id: 2 } as Service);
 		expect(LabelsRepositoryMock.findMock).toHaveBeenCalledTimes(1);
+	});
+
+	it('Labels not present in Service', () => {
+		const label = Label.create('name');
+		const labelNotPresent = Label.create('not present');
+		LabelsRepositoryMock.findMock.mockReturnValue(new Promise(() => [label]));
+		try {
+			Container.get(LabelsService).verifyLabels([labelNotPresent], { id: 2 } as Service);
+		} catch (e){
+			expect(e).toBeInstanceOf(MOLErrorV2);
+		}
 	});
 });
