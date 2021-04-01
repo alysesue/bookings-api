36dfc26e353884383641bd2baa179d8ceddcd812
Darrel <darrel@ip-192-168-1-17.ap-southeast-1.compute.internal>
2021-04-01 12:34:12 +0800

    [DC] fix test function

--- src/components/labels/__tests__/labels.service.spec.ts
+++ src/components/labels/__tests__/labels.service.spec.ts
@@ -3,26 +3,32 @@ import { LabelsRepository } from '../labels.repository';
 import { LabelsRepositoryMock } from '../__mocks__/labels.repository.mock';
 import { Label, Service } from '../../../models/entities';
 import { LabelsService } from '../labels.service';
+import { LabelRequestModel } from '../label.apicontract';
+import { LabelsMapper } from '../labels.mapper';
 
 describe('Test labels service', () => {
 	beforeAll(() => {
 		Container.bind(LabelsRepository).to(LabelsRepositoryMock);
+		Container.bind(LabelsMapper);
 	});
 	afterEach(() => {
 		jest.resetAllMocks();
 	});
 
 	it('Should verify if labels are present in Service & remove duplication', async () => {
-		const label = Label.create('name');
-		LabelsRepositoryMock.findMock.mockReturnValue([label]);
+		const label = 'name';
+		const duplicateLabels: LabelRequestModel[] = [{ label }, { label }];
+		const deduplicatedLabel = Container.get(LabelsMapper).mapToLabels(duplicateLabels);
+
+		LabelsRepositoryMock.findMock.mockReturnValue(deduplicatedLabel);
 		let resLabel;
 		try {
-			resLabel = await Container.get(LabelsService).verifyLabels([label, label], { id: 2 } as Service);
+			resLabel = await Container.get(LabelsService).verifyLabels(deduplicatedLabel, { id: 2 } as Service);
 		} catch (e) {
 			expect(e).toBeFalsy();
 		}
 		expect(LabelsRepositoryMock.findMock).toHaveBeenCalledTimes(1);
-		expect(resLabel).toStrictEqual([label]);
+		expect(resLabel).toStrictEqual(deduplicatedLabel);
 	});
 
 	it('Service dont have any label', async () => {
@@ -32,16 +38,6 @@ describe('Test labels service', () => {
 			expect(LabelsRepositoryMock.findMock).toHaveBeenCalledTimes(1);
 		} catch (err) {
 			expect(err.message).toBe('Service does not have any labels');
-		}
-	});
-
-	it('Labels not present in Service', async () => {
-		const label = Label.create('name');
-		try {
-			await Container.get(LabelsService).verifyLabels([], { id: 2 } as Service);
-			expect(LabelsRepositoryMock.findMock).toHaveBeenCalledTimes(1);
-		} catch (err) {
-			expect(err.message).toBe(`This label is not present: ${label}`);
 		}
 	});
 });
