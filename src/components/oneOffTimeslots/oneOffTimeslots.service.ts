b17cee0af4405696813a8460e6c928e6718286e2
Adrien L <adrien@ip-192-168-1-100.ap-southeast-1.compute.internal>
2021-04-12 17:15:04 +1000

    [AL] Buiness rule add

--- src/components/oneOffTimeslots/oneOffTimeslots.service.ts
+++ src/components/oneOffTimeslots/oneOffTimeslots.service.ts
@@ -8,6 +8,7 @@ import { OneOffTimeslotsActionAuthVisitor } from './oneOffTimeslots.auth';
 import { ErrorCodeV2, MOLErrorV2 } from 'mol-lib-api-contract';
 import { LabelsService } from '../labels/labels.service';
 import { OneOffTimeslotsMapper } from './oneOffTimeslots.mapper';
+import { OneOffTimeslotsValidation } from './oneOffTimeslots.validation';
 
 @InRequestScope
 export class OneOffTimeslotsService {
@@ -19,6 +20,8 @@ export class OneOffTimeslotsService {
 	private userContext: UserContext;
 	@Inject
 	private labelsService: LabelsService;
+	@Inject
+	private oneOffTimeslotsValidation: OneOffTimeslotsValidation;
 
 	private async verifyActionPermission(entity: OneOffTimeslot): Promise<void> {
 		const authGroups = await this.userContext.getAuthGroups();
@@ -29,27 +32,11 @@ export class OneOffTimeslotsService {
 		}
 	}
 
-	private validate(entity: OneOffTimeslot) {
-		const { startDateTime, endDateTime, title, description } = entity;
-		if (startDateTime.getTime() >= endDateTime.getTime()) {
-			throw new MOLErrorV2(ErrorCodeV2.SYS_INVALID_PARAM).setMessage(`Start time must be less than end time.`);
-		}
-		if (title && title.length > 100) {
-			throw new MOLErrorV2(ErrorCodeV2.SYS_INVALID_PARAM).setMessage(`Title should be max 100 characters`);
-		}
-		if (description && description.length > 4000) {
-			throw new MOLErrorV2(ErrorCodeV2.SYS_INVALID_PARAM).setMessage(`Description should be max 4000 characters`);
-		}
-	}
-
 	public async save(request: OneOffTimeslotRequest): Promise<OneOffTimeslot> {
 		const serviceProvider = await this.serviceProvidersService.getServiceProvider(request.serviceProviderId);
 		const labels = await this.labelsService.verifyLabels(request.labelIds, serviceProvider.serviceId);
-
 		const entity = OneOffTimeslotsMapper.mapToOneOffTimeslots(request, serviceProvider, labels);
-
-		this.validate(entity);
-
+		await this.oneOffTimeslotsValidation.validate(entity);
 		await this.verifyActionPermission(entity);
 		await this.oneOffTimeslotsRepo.save(entity);
 		return entity;
