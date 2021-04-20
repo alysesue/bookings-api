ceb7f0493687b2b48636578706c312b67595e345
mrozen <mrozen@palo-it.com>
2021-04-20 16:17:44 +1000

    Added title and description tests - mapper

--- src/components/timeslots/__tests__/timeslots.mapper.spec.ts
+++ src/components/timeslots/__tests__/timeslots.mapper.spec.ts
@@ -85,6 +85,96 @@ describe('Timeslots Mapper', () => {
 		expect(totalCapacity).toBe(6);
 		expect(totalBooked).toBe(0);
 	});
+
+	it('should map service provider timeslot title and description - admin side', () => {
+		const entry = new AvailableTimeslotProviders();
+		entry.startTime = new Date(2020, 8, 26, 8, 0).getTime();
+		entry.endTime = new Date(2020, 8, 26, 8, 30).getTime();
+
+		const serviceProvider1 = ServiceProvider.create('Timmy', 1);
+		serviceProvider1.id = 1;
+		const serviceProvider2 = ServiceProvider.create('Andy', 1);
+		serviceProvider2.id = 2;
+
+		const map = new Map<ServiceProvider, TimeslotWithCapacity>();
+		map.set(serviceProvider1, {
+			startTimeNative: entry.startTime,
+			endTimeNative: entry.endTime,
+			capacity: 1,
+			title: 'Title Test - serviceProvider1',
+			description: 'Description Test - serviceProvider1',
+		} as TimeslotWithCapacity);
+		map.set(serviceProvider2, {
+			startTimeNative: entry.startTime,
+			endTimeNative: entry.endTime,
+			capacity: 5,
+			title: 'Title Test - serviceProvider2',
+			description: 'Description Test - serviceProvider2',
+		} as TimeslotWithCapacity);
+		entry.setRelatedServiceProviders(map);
+
+		const timeslotServiceProviders = Array.from(entry.getTimeslotServiceProviders());
+		const adminMock = User.createAdminUser({
+			molAdminId: 'd080f6ed-3b47-478a-a6c6-dfb5608a199d',
+			userName: 'UserName',
+			email: 'test@email.com',
+			name: 'Name',
+		});
+		UinFinConfigurationMock.canViewPlainUinFin.mockReturnValue(false);
+
+		const mapper = Container.get(TimeslotsMapper);
+		const res = mapper.mapTimeslotServiceProviders(timeslotServiceProviders, {
+			user: adminMock,
+			authGroups: [],
+		});
+
+		const [spResponse] = res;
+		expect(spResponse.length).toBe(2);
+		expect(spResponse[0].eventTitle).toBe('Title Test - serviceProvider1');
+		expect(spResponse[1].eventTitle).toBe('Title Test - serviceProvider2');
+		expect(spResponse[0].eventDescription).toBe('Description Test - serviceProvider1');
+		expect(spResponse[1].eventDescription).toBe('Description Test - serviceProvider2');
+	});
+
+	it('should map service provider timeslot title and description - citizen side', () => {
+		const entry = new AvailableTimeslotProviders();
+		entry.startTime = new Date(2020, 8, 26, 8, 0).getTime();
+		entry.endTime = new Date(2020, 8, 26, 8, 30).getTime();
+
+		const serviceProvider1 = ServiceProvider.create('Timmy', 1);
+		serviceProvider1.id = 1;
+		const serviceProvider2 = ServiceProvider.create('Andy', 1);
+		serviceProvider2.id = 2;
+
+		const map = new Map<ServiceProvider, TimeslotWithCapacity>();
+		map.set(serviceProvider1, {
+			startTimeNative: entry.startTime,
+			endTimeNative: entry.endTime,
+			capacity: 1,
+			title: 'Title Test - serviceProvider1',
+			description: 'Description Test - serviceProvider1',
+		} as TimeslotWithCapacity);
+		map.set(serviceProvider2, {
+			startTimeNative: entry.startTime,
+			endTimeNative: entry.endTime,
+			capacity: 5,
+			title: 'Title Test - serviceProvider2',
+			description: 'Description Test - serviceProvider2',
+		} as TimeslotWithCapacity);
+		entry.setRelatedServiceProviders(map);
+
+		const timeslotServiceProviders = Array.from(entry.getTimeslotServiceProviders());
+		UinFinConfigurationMock.canViewPlainUinFin.mockReturnValue(false);
+
+		const mapper = Container.get(TimeslotsMapper);
+		const response = mapper.mapCitizenTimeslotServiceProviders(timeslotServiceProviders);
+
+		expect(response[0].length).toBe(2);
+		expect(response[0][1].eventTitle).toBe('Title Test - serviceProvider2');
+		expect(response[0][0].eventTitle).toBe('Title Test - serviceProvider1');
+		expect(response[0][0].eventDescription).toBe('Description Test - serviceProvider1');
+		expect(response[0][1].eventDescription).toBe('Description Test - serviceProvider2');
+	});
 });
 
 class UinFinConfigurationMock implements Partial<UinFinConfiguration> {
