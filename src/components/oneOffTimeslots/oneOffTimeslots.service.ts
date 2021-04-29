18d643b35f19d08e5156eeeb593e43f0c54e7038
mrozen <mrozen@palo-it.com>
2021-04-29 11:57:17 +1000

    updated message to better describe the error

--- src/components/oneOffTimeslots/oneOffTimeslots.service.ts
+++ src/components/oneOffTimeslots/oneOffTimeslots.service.ts
@@ -58,7 +58,7 @@ export class OneOffTimeslotsService {
 		};
 		const slotAvailableArr = await this.oneOffTimeslotsRepo.search(searchRequest);
 		if (slotAvailableArr.length){
-			throw new MOLErrorV2(ErrorCodeV2.SYS_INVALID_PARAM).setMessage(`Slot cannot be created as a slot with the same date and time already exist`);
+			throw new MOLErrorV2(ErrorCodeV2.SYS_INVALID_PARAM).setMessage(`Slot cannot be created as it overlaps with an existing slot.`);
 		}
 
 		await this.oneOffTimeslotsValidation.validate(entity);
