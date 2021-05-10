9ea360a3bb35dde5760f49f55defffc5bb0f1d94
Will <wnogueira@palo-it.com>
2021-05-10 13:38:07 +1000

    test fix

--- src/components/oneOffTimeslots/__mocks__/oneOffTimeslots.mock.ts
+++ src/components/oneOffTimeslots/__mocks__/oneOffTimeslots.mock.ts
@@ -2,9 +2,9 @@ import { OneOffTimeslotsRepository } from '../oneOffTimeslots.repository';
 import { OneOffTimeslot } from '../../../models/entities';
 
 export class OneOffTimeslotsRepositoryMock implements Partial<OneOffTimeslotsRepository> {
-	public static searchBookings = jest.fn<Promise<OneOffTimeslot[]>, any>();
+	public static search = jest.fn<Promise<OneOffTimeslot[]>, any>();
 
 	public async search(...params): Promise<any> {
-		return await OneOffTimeslotsRepositoryMock.searchBookings(...params);
+		return await OneOffTimeslotsRepositoryMock.search(...params);
 	}
 }
