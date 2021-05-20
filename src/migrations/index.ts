2c5b841246a5b05541f14ac3365fb6635186c00d
Adrien L <adrien@ip-192-168-1-107.ap-southeast-1.compute.internal>
2021-05-20 13:08:58 +1000

    Rename database categorie to labelCategorie

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -16,4 +16,5 @@ export { OneOfTimeslotsTitle1618189257455 } from './1618189257455-OneOfTimeslots
 export { ServiceFlagSendEmail1619141888356 } from './1619141888356-ServiceFlagSendEmail';
 export { EmailSuffix1619458019731 } from './1619458019731-EmailSuffix';
 export { CapacityForScheduleForm1620205273706 } from './1620205273706-capacityForScheduleForm1';
+export { LabelCategory1621479880832 } from './1621479880832-LabelCategory';
 
