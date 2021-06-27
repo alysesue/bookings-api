bab4b652cffc7d9b1584a339d4826f7947204ccf
mrozen <mrozen@palo-it.com>
2021-06-27 21:36:54 +1000

    migration file

--- src/migrations/1624598751619-ServiceProviderAliasName.ts
+++ src/migrations/1624598751619-ServiceProviderAliasName.ts
@@ -1,14 +1,13 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class ServiceProviderAliasName1624598751619 implements MigrationInterface {
-    name = 'ServiceProviderAliasName1624598751619'
+	name = 'ServiceProviderAliasName1624598751619';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "service_provider" ADD "_aliasName" character varying(100)`);
-    }
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(`ALTER TABLE "service_provider" ADD "_aliasName" character varying(100)`);
+	}
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "service_provider" DROP COLUMN "_aliasName"`);
-    }
-
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(`ALTER TABLE "service_provider" DROP COLUMN "_aliasName"`);
+	}
 }
