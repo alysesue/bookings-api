326026a040ad692fb154373eccdf013cdb3ea912
Darrel Cai <darrel_cai@tech.gov.sg>
2021-07-02 09:09:47 +0800

    added validation for NRIC

--- src/tools/validator.ts
+++ src/tools/validator.ts
@@ -1,3 +1,5 @@
+import { getUinfinChecksum } from 'mol-lib-api-contract/utils';
+
 const _ = require('lodash');
 export const isSGUinfin = (uinFinString?: string): { pass: boolean; message?: string } => {
 	if (!_.isString(uinFinString)) {
@@ -5,6 +7,14 @@ export const isSGUinfin = (uinFinString?: string): { pass: boolean; message?: st
 	}
 	const pattern = '^[XSTFG]\\d{7}[A-Z]$';
 	if (uinFinString.match(pattern) === null) {
+		return { pass: false, message: `should be a valid NRIC/FIN` };
+	}
+
+	// TO BE REMOVED: cater for X series
+	if (uinFinString.charAt(0) === 'X') return { pass: true };
+
+	const checkSum = getUinfinChecksum(uinFinString);
+	if (uinFinString.charAt(8) !== checkSum) {
 		return { pass: false, message: `should be a valid NRIC/FIN` };
 	}
 
