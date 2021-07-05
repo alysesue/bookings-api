eda62f1ca9fdfd53a2ad6fe8a37f8b75ef90a08d
mrozen <mrozen@palo-it.com>
2021-07-05 10:14:07 +1000

    enum for notifications

---
+++ src/models/notifications.ts
@@ -0,0 +1,14 @@
+export enum EmailNotificationTemplates {
+    CreatedByCitizenSentToCitizen = 1,
+    UpdatedByCitizenSentToCitizen,
+    CancelledByCitizenSentToCitizen,
+    CreatedByCitizenSentToServiceProvider ,
+    UpdatedByCitizenSentToServiceProvider,
+    CancelledByCitizenSentToServiceProvider,
+    CreatedByServiceProviderSentToCitizen,
+    UpdatedByServiceProviderSentToCitizen,
+    CancelledByServiceProviderSentToCitizen,
+    CreatedByServiceProviderSentToServiceProvider , // currently undefined
+    UpdatedByServiceProviderSentToServiceProvider,
+    CancelledByServiceProviderSentToServiceProvider,
+}
