ed33fe1ece835f71aa7f934bbcb6e4fe7aa8af12
mrozen <mrozen@palo-it.com>
2021-07-08 15:54:43 +1000

    lint

--- src/components/serviceNotificationTemplate/__tests__/serviceNotificationTemplate.service.spec.ts
+++ src/components/serviceNotificationTemplate/__tests__/serviceNotificationTemplate.service.spec.ts
@@ -3,8 +3,8 @@ import { IdHasher } from '../../../infrastructure/idHasher';
 import { ServiceNotificationTemplate } from '../../../models';
 import { ServiceNotificationTemplateRepository } from '../serviceNotificationTemplate.repository';
 import { IdHasherMock } from '../../../infrastructure/__mocks__/idHasher.mock';
-import {ServiceNotificationTemplateService} from "../serviceNotificationTemplate.service";
-import {ServiceNotificationTemplateRequest} from "../serviceNotificationTemplate.apicontract";
+import { ServiceNotificationTemplateService } from '../serviceNotificationTemplate.service';
+import { ServiceNotificationTemplateRequest } from '../serviceNotificationTemplate.apicontract';
 
 describe('Test the service notification template service', () => {
 	beforeAll(() => {
@@ -22,35 +22,35 @@ describe('Test the service notification template service', () => {
 
 	const template = new ServiceNotificationTemplate();
 	template.emailTemplateType = 2;
-	template.htmlTemplate = "testings notification template"
-    template.id = 123;
+	template.htmlTemplate = 'testings notification template';
+	template.id = 123;
 	template.serviceId = 1;
 
-    it('should get an email notification template', async () => {
-        ServiceNotificationTemplateRepositoryMock.getTemplateMock.mockReturnValue(template);
-        const result = await Container.get(ServiceNotificationTemplateService).getEmailNotificationTemplate(1, 2);
+	it('should get an email notification template', async () => {
+		ServiceNotificationTemplateRepositoryMock.getTemplateMock.mockReturnValue(template);
+		const result = await Container.get(ServiceNotificationTemplateService).getEmailNotificationTemplate(1, 2);
 
-        expect(result).toStrictEqual(template);
-    });
+		expect(result).toStrictEqual(template);
+	});
 
-    it('should add an email notification template', async () => {
-        const request = new ServiceNotificationTemplateRequest(4, "irrelevant");
-        ServiceNotificationTemplateRepositoryMock.saveMock.mockReturnValue(template);
-        const result = await Container.get(ServiceNotificationTemplateService).addEmailTemplate(1, request);
+	it('should add an email notification template', async () => {
+		const request = new ServiceNotificationTemplateRequest(4, 'irrelevant');
+		ServiceNotificationTemplateRepositoryMock.saveMock.mockReturnValue(template);
+		const result = await Container.get(ServiceNotificationTemplateService).addEmailTemplate(1, request);
 
-        expect(result).toStrictEqual(template);
-    });
+		expect(result).toStrictEqual(template);
+	});
 });
 
 class ServiceNotificationTemplateRepositoryMock implements Partial<ServiceNotificationTemplateRepository> {
-    public static saveMock = jest.fn();
-    public static getTemplateMock = jest.fn();
+	public static saveMock = jest.fn();
+	public static getTemplateMock = jest.fn();
 
-    public async save(){
-        return ServiceNotificationTemplateRepositoryMock.saveMock();
-    }
+	public async save() {
+		return ServiceNotificationTemplateRepositoryMock.saveMock();
+	}
 
-    public async getTemplate(){
-        return ServiceNotificationTemplateRepositoryMock.getTemplateMock();
-    }
+	public async getTemplate() {
+		return ServiceNotificationTemplateRepositoryMock.getTemplateMock();
+	}
 }
