9bec9cecf70e2361cea45c4fe2f4c87efdb22c19
mrozen <mrozen@palo-it.com>
2021-07-09 11:09:13 +1000

    added unit tests to serviceNotificationTemplate.repository.spec.ts

--- src/components/serviceNotificationTemplate/__tests__/serviceNotificationTemplate.repository.spec.ts
+++ src/components/serviceNotificationTemplate/__tests__/serviceNotificationTemplate.repository.spec.ts
@@ -1,60 +1,65 @@
-// import { ServiceNotificationTemplate } from '../../../models';
-// import { Container } from 'typescript-ioc';
-// import { ServiceNotificationTemplateRepository } from '../serviceNotificationTemplate.repository';
-// import { TransactionManager } from '../../../core/transactionManager';
-// import { TransactionManagerMock } from '../../../core/__mocks__/transactionManager.mock';
-// import { UserContext } from '../../../infrastructure/auth/userContext';
-// import { UserContextMock } from '../../../infrastructure/auth/__mocks__/userContext';
-//
-// describe('Test the service notification template repository', () => {
-// 	beforeAll(() => {
-// 		jest.resetAllMocks();
-// 		Container.bind(TransactionManager).to(TransactionManagerMock);
-// 		Container.bind(UserContext).to(UserContextMock);
-// 	});
-//
-// 	beforeEach(() => {
-// 		jest.resetAllMocks();
-// 		UserContextMock.getAuthGroups.mockReturnValue(Promise.resolve([]));
-// 	});
-//
-// 	const template = new ServiceNotificationTemplate();
-// 	template.emailTemplateType = 2;
-// 	template.htmlTemplate = 'testings notification template';
-// 	template.serviceId = 1;
-//
-// 	const expectedTemplate = new ServiceNotificationTemplate();
-// 	expectedTemplate.emailTemplateType = 2;
-// 	expectedTemplate.htmlTemplate = 'testings notification template';
-// 	expectedTemplate.serviceId = 1;
-// 	expectedTemplate.id = 1;
-//
-// 	it('should save an email notification template', async () => {
-// 		TransactionManagerMock.save.mockImplementation(() => Promise.resolve(expectedTemplate));
-// 		const repository = Container.get(ServiceNotificationTemplateRepository);
-// 		const result = await repository.save(template);
-// 		expect(result).toEqual(expectedTemplate);
-// 	});
-//
-// 	it('should get an email notification template', async () => {
-// 		const queryMock: {
-// 			where: jest.Mock;
-// 			leftJoin: jest.Mock;
-// 			leftJoinAndSelect: jest.Mock;
-// 			getOne: jest.Mock<Promise<ServiceNotificationTemplate>, any>;
-// 		} = {
-// 			where: jest.fn(),
-// 			leftJoin: jest.fn(),
-// 			leftJoinAndSelect: jest.fn(),
-// 			getOne: jest.fn<Promise<ServiceNotificationTemplate>, any>(),
-// 		};
-//
-// 		queryMock.getOne.mockImplementation(() => Promise.resolve(template));
-//
-// 		const repository = Container.get(ServiceNotificationTemplateRepository);
-// 		const result = await repository.getTemplate(1, 2);
-// 		console.log('result', result);
-// 		// expect(result).toBeDefined();
-// 		// expect(queryMock.getOne).toBeCalled();
-// 	});
-// });
+import { ServiceNotificationTemplate } from '../../../models';
+import { Container } from 'typescript-ioc';
+import { ServiceNotificationTemplateRepository } from '../serviceNotificationTemplate.repository';
+import { TransactionManager } from '../../../core/transactionManager';
+import { TransactionManagerMock } from '../../../core/__mocks__/transactionManager.mock';
+import { UserContext } from '../../../infrastructure/auth/userContext';
+import { UserContextMock } from '../../../infrastructure/auth/__mocks__/userContext';
+
+describe('Test the service notification template repository', () => {
+	beforeAll(() => {
+		jest.resetAllMocks();
+		Container.bind(TransactionManager).to(TransactionManagerMock);
+		Container.bind(UserContext).to(UserContextMock);
+	});
+
+	beforeEach(() => {
+		jest.resetAllMocks();
+		UserContextMock.getAuthGroups.mockReturnValue(Promise.resolve([]));
+	});
+
+	// const userMock = User.createAdminUser({
+	//     molAdminId: 'd080f6ed-3b47-478a-a6c6-dfb5608a199d',
+	//     userName: 'UserName',
+	//     email: 'test@email.com',
+	//     name: 'Name',
+	//     agencyUserId: 'ABC12',
+	// });
+
+	const template = new ServiceNotificationTemplate();
+	template.emailTemplateType = 2;
+	template.htmlTemplate = 'testings notification template';
+	template.serviceId = 1;
+
+	const expectedTemplate = new ServiceNotificationTemplate();
+	expectedTemplate.emailTemplateType = 2;
+	expectedTemplate.htmlTemplate = 'testings notification template';
+	expectedTemplate.serviceId = 1;
+	expectedTemplate.id = 1;
+
+	it('should save an email notification template', async () => {
+		TransactionManagerMock.save.mockImplementation(() => Promise.resolve(expectedTemplate));
+		const repository = Container.get(ServiceNotificationTemplateRepository);
+		const result = await repository.save(template);
+		expect(result).toEqual(expectedTemplate);
+	});
+
+	it('should get an email notification template', async () => {
+		const queryBuilderMock: {
+			where: jest.Mock;
+			getOne: jest.Mock<Promise<ServiceNotificationTemplate>, any>;
+		} = {
+			where: jest.fn(),
+			getOne: jest.fn<Promise<ServiceNotificationTemplate>, any>(),
+		};
+        queryBuilderMock.where.mockImplementation(() => queryBuilderMock);
+		queryBuilderMock.getOne.mockImplementation(() => Promise.resolve(template));
+        TransactionManagerMock.createQueryBuilder.mockImplementation(() => queryBuilderMock);
+
+		const repository = Container.get(ServiceNotificationTemplateRepository);
+		const result = await repository.getTemplate(1, 2);
+        expect(queryBuilderMock.getOne).toBeCalled();
+        expect(result).toBeDefined();
+		expect(result.htmlTemplate).toEqual('testings notification template')
+	});
+});
