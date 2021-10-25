434a4a320aae9152d558f1597f3d5b42d8674645
Leong Wei Jian <weijian19391@gmail.com>
2021-10-25 17:57:15 +0800

    [GCC][weijian] update test for sending email

--- src/components/notifications/__tests__/notification.service.spec.ts
+++ src/components/notifications/__tests__/notification.service.spec.ts
@@ -1,24 +1,19 @@
+import { getConfig } from './../../../config/app-config';
 import { Container } from 'typescript-ioc';
 import { NotificationsService } from '../notifications.service';
 import { CreateEmail } from 'mol-lib-api-contract/notification/mail';
 import { ErrorCodeV2, MOLErrorV2 } from 'mol-lib-api-contract/error';
-import { mailer } from '../../../config/mailer';
+// import { mailer } from '../../../config/mailer';
+import axios from 'axios';
 
-jest.mock('../../../config/mailer');
-
+jest.mock('axios');
 jest.mock('../../../config/app-config', () => {
 	const configMock = {
 		isLocal: true,
-		molNotification: { url: '' },
-		mailer: {
-			smtpHost: 'email-smtp.us-east-1.amazonaws.com',
-			smtpPort: '',
-			smtpSecure: '',
-			smtpUseAuth: '',
-			smtpAuthUsername: '',
-			smtpAuthPassword: '',
+		awsLambdaMailer: {
+			baseUrl: 'https:www.google.com',
+			apikey: '1234567',
 		},
-
 		email: {
 			mol: {
 				sender: '',
@@ -33,37 +28,19 @@ jest.mock('../../../config/app-config', () => {
 
 // tslint:disable-next-line:no-big-function
 describe('Send email', () => {
-	const mailerMock = {
-		sendMail: jest.fn(),
-	};
+	const axiosMock = axios.request as jest.Mock;
 
 	beforeEach(() => {
 		jest.resetAllMocks();
-		mailerMock.sendMail.mockImplementation(() =>
+		axiosMock.mockImplementation(() =>
 			Promise.resolve({
-				accepted: ['success@foo.com'],
-				rejected: ['reject@foo.com'],
-				messageId: 'messageId',
+				data: JSON.stringify({
+					accepted: ['success@foo.com'],
+					rejected: [],
+					messageId: 'messageId',
+				}),
 			}),
 		);
-
-		(mailer as jest.Mock).mockImplementation(() => Promise.resolve(mailerMock));
-	});
-
-	it('should send mail to single recipient', async () => {
-		const options: CreateEmail.Domain.CreateEmailRequestApiDomain = {
-			from: 'from@foo.com',
-			to: ['to@foo.com'],
-			subject: 'Single recipient',
-			html: '<h1>Single recipient</h1>',
-		};
-		const expectedOptions = {
-			...options,
-			to: options.to.join(','),
-		};
-		const instance = await Container.get(NotificationsService);
-		await instance.sendEmail(options);
-		expect(mailerMock.sendMail).toHaveBeenCalledWith(expectedOptions);
 	});
 
 	it('should throw error when email is invalid', async () => {
@@ -78,25 +55,9 @@ describe('Send email', () => {
 		await expect(instance.sendEmail(options)).rejects.toEqual(invalidEmailError);
 	});
 
-	it('should send mail to multiple recipients', async () => {
-		const options: CreateEmail.Domain.CreateEmailRequestApiDomain = {
-			from: 'from@au.foo.com',
-			to: ['to@foo.com', 'anotherto@foo.com'],
-			subject: 'Multiple recipients',
-			html: '<h1>Multiple recipients</h1>',
-		};
-		const expectedOptions = {
-			...options,
-			to: options.to.join(','),
-		};
-		const instance = await Container.get(NotificationsService);
-		await instance.sendEmail(options);
-		expect(mailerMock.sendMail).toHaveBeenCalledWith(expectedOptions);
-	});
-
 	it('should throw error when any email is invalid', async () => {
 		const options: CreateEmail.Domain.CreateEmailRequestApiDomain = {
-			to: ['INVALID EMAIL', 'valid@email.com'],
+			to: ['valid@email.com', 'INVALID EMAIL'],
 			subject: 'Single recipient',
 			html: '<h1>Single recipient</h1>',
 		};
@@ -104,5 +65,44 @@ describe('Send email', () => {
 		const invalidEmailError = new MOLErrorV2(ErrorCodeV2.SYS_GENERIC).setMessage('Invalid email address');
 		const instance = await Container.get(NotificationsService);
 		await expect(instance.sendEmail(options)).rejects.toEqual(invalidEmailError);
+	});
+
+	it('should send mail to single recipient', async () => {
+		const options: CreateEmail.Domain.CreateEmailRequestApiDomain = {
+			from: 'from@foo.com',
+			to: ['to@foo.com'],
+			subject: 'Single recipient',
+			html: '<h1>Single recipient</h1>',
+		};
+		const expectedOptions = {
+			method: 'post',
+			url: getConfig().awsLambdaMailer.baseUrl,
+			data: JSON.stringify({ ...options, to: options.to.join(',') }),
+			headers: { 'x-api-key': getConfig().awsLambdaMailer.apikey },
+		};
+
+		const instance = await Container.get(NotificationsService);
+		await instance.sendEmail(options);
+		expect(axios.request).toHaveBeenCalledWith(expectedOptions);
+	});
+
+	it('should send mail to multiple recipients', async () => {
+		const options: CreateEmail.Domain.CreateEmailRequestApiDomain = {
+			from: 'from@au.foo.com',
+			to: ['to@foo.com', 'anotherto@foo.com'],
+			subject: 'Multiple recipients',
+			html: '<h1>Multiple recipients</h1>',
+		};
+
+		const expectedOptions = {
+			method: 'post',
+			url: getConfig().awsLambdaMailer.baseUrl,
+			data: JSON.stringify({ ...options, to: options.to.join(',') }),
+			headers: { 'x-api-key': getConfig().awsLambdaMailer.apikey },
+		};
+
+		const instance = await Container.get(NotificationsService);
+		await instance.sendEmail(options);
+		expect(axios.request).toHaveBeenCalledWith(expectedOptions);
 	});
 });
