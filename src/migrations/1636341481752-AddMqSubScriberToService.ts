3ad5e141770a5283b5789ed98f516543dcc2e91e
chenwei-choo <chenwei.choo@ufinity.com>
2021-11-08 11:22:16 +0800

    [MOL-8640][CCW|BC] Add MqSubscriber to service (migration scripts)

---
+++ src/migrations/1636341481752-AddMqSubScriberToService.ts
@@ -0,0 +1,29 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddMqSubScriberToService1636341481752 implements MigrationInterface {
+	name = 'AddMqSubScriberToService1636341481752';
+
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(`CREATE TYPE "service__mqsubscriber_enum" AS ENUM('lifesg')`);
+		await queryRunner.query(
+			`ALTER TABLE "service" ADD "_mqSubscriber" "service__mqsubscriber_enum" array NOT NULL DEFAULT '{}'`,
+		);
+		await queryRunner.query(
+			`ALTER TABLE "event" ALTER COLUMN "_firstStartDateTime" SET DEFAULT '"2020-01-01T14:00:00.000Z"'`,
+		);
+		await queryRunner.query(
+			`ALTER TABLE "event" ALTER COLUMN "_lastEndDateTime" SET DEFAULT '"2050-01-01T14:00:00.000Z"'`,
+		);
+	}
+
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(
+			`ALTER TABLE "event" ALTER COLUMN "_lastEndDateTime" SET DEFAULT '2050-01-01 14:00:00'`,
+		);
+		await queryRunner.query(
+			`ALTER TABLE "event" ALTER COLUMN "_firstStartDateTime" SET DEFAULT '2020-01-01 14:00:00'`,
+		);
+		await queryRunner.query(`ALTER TABLE "service" DROP COLUMN "_mqSubscriber"`);
+		await queryRunner.query(`DROP TYPE "service__mqsubscriber_enum"`);
+	}
+}
