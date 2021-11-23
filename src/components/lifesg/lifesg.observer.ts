d1a7de5e06df62d948671d64f7e07a2f8fc80fbd
Leong Wei Jian <weijian19391@gmail.com>
2021-11-23 10:56:45 +0800

    [MOL-8640][weijian] update comments

--- src/components/lifesg/lifesg.observer.ts
+++ src/components/lifesg/lifesg.observer.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import { BookingStatus } from '../../models';
 import { Inject, InRequestScope } from 'typescript-ioc';
 import { IObserver, ISubject } from '../../infrastructure/observer';
@@ -11,16 +12,16 @@ export class LifeSGObserver implements IObserver {
 	@Inject
 	private lifeSGMQSerice: LifeSGMQSerice;
 	/**
-	 * This method will send an accepted or cancelled booking to LifeSG's appointment service. So a booking entity on BSG is an appointment entity in LifeSG
-	 * Over at LifeSG, what they will do with the booking data is to display it in the LifeSG mobile app, so that LifeSG users can view bookings on the LifeSG mobile app
-	 * The following function will check that booking.service.mqSubscriber contains LifeSG enum, hence if we want bookings to be made to LifeSG, we will need to set the service level setting mqSubscriber to {lifesg}
+	 * This method will send an accepted or cancelled booking to LifeSG's appointment service, a booking entity on BSG is an appointment entity in LifeSG appointment service
+	 * Over at LifeSG, what they will do with the booking data is to display it in the LifeSG mobile app, so that LifeSG users can view bookings made in BSG on the LifeSG mobile app
+	 * The following method will check that booking.service.mqSubscriber contains LifeSG enum, hence if we want bookings to be piped to LifeSG, we will need to set the service level setting mqSubscriber to {lifesg}
 	 *
 	 * **IMPORTANT NOTEs**
-	 * 1. As of Nov 2021, only HDB-VC service requires their bookings to be piped to LifeSG, hence we did a quick fix to hardcode that all bookings made will be piped to LifeSG as a HDB-VC booking: `lifeSGMQService.send(...., AppointmentAgency.HDBVC_BSG)`
+	 * 1. As of Nov 2021, only HDB-VC service requires their bookings to be piped to LifeSG, hence we did a hardcode such that all bookings with service level setting mqSubscriber to {lifesg} will be piped to LifeSG as a HDB-VC booking: `lifeSGMQService.send(...., AppointmentAgency.HDBVC_BSG)`
 	 * In future when we have other organisations who wants their booking to be piped to LifeSG, we will have to remove this hardcode, else their bookings will still be piped to LifeSG as HDB-VC booking.
 	 *
-	 * 2. When comes to rescheduling, things gets a little more complicated because BSG bookings might have pending status, but LifeSG side does not have such a status for their appointments
-	 * Currently not an issue because we only have 1 use case, and that's HDB-VC, and HDB-VC is auto accept flow, so their bookings will never become pending
+	 * 2. When comes to rescheduling, things gets a little more complicated because BSG bookings might have pending status, but LifeSG side does not cater to an pending status.
+	 * Currently not an issue because we only have 1 use case: HDB-VC, and HDB-VC is auto accept flow, so their bookings will never become pending even when rescheduled by citizens
 	 * In future when we have pending bookings to be sent to LifeSG, BSG and LifeSG will have to sit down again to discuss how to handle pending bookings
 	 */
 	public async update(subject: ISubject<any>): Promise<void> {
